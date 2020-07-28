cbea35015e86e0303dd82b08ec45621e23e2e62e
yannaing-lynn <rghtbossy@gmail.com>
2020-07-28 15:27:08 +0630

    full screen ok

--- app/src/main/java/com/snatik/matches/MainActivity.kt
+++ app/src/main/java/com/snatik/matches/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.snatik.matches
 
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.FragmentActivity
 import android.view.View
-import android.view.WindowManager
 import android.widget.ImageView
 import com.snatik.matches.common.Shared
 import com.snatik.matches.engine.Engine
@@ -14,11 +15,38 @@ import com.snatik.matches.events.ui.BackGameEvent
 import com.snatik.matches.ui.PopupManager
 import com.snatik.matches.utils.Utils
 
+
 class MainActivity : FragmentActivity() {
     private var mBackgroundImage: ImageView? = null
+    private var currentApiVersion = 0
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        currentApiVersion = Build.VERSION.SDK_INT
+
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+        // This work only for android 4.4+
+
+        // This work only for android 4.4+
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+            window.decorView.systemUiVisibility = flags
+
+            // Code below is to handle presses of Volume up or Volume down.
+            // Without this, after pressing volume buttons, the navigation bar will
+            // show up and won't hide
+            val decorView = window.decorView
+            decorView
+                    .setOnSystemUiVisibilityChangeListener { visibility ->
+                        if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                            decorView.systemUiVisibility = flags
+                        }
+                    }
+        }
         super.onCreate(savedInstanceState)
         Shared.context = applicationContext
         Shared.engine = Engine.Companion.getInstance()
@@ -36,6 +64,18 @@ class MainActivity : FragmentActivity() {
         ScreenController.Companion.getInstance()!!.openScreen(Screen.MENU)
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT && hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
     override fun onDestroy() {
         Shared.engine!!.stop()
         super.onDestroy()
